0db879b9648914bed407de7a6721c2505d4350e9
Favour Abilogun-Wole Oluwasayo <favyabils125@gmail.com>
2024-03-14 18:30:29 +0100

    Change addded

--- Homework/OOP.Q1.py
+++ Homework/OOP.Q1.py
@@ -6,12 +6,13 @@ class Employee:
         self.emp_department = emp_department
     
     def  calculate_emp_salary(self, salary, hours_worked):
-        self.salary = self.emp_salary
-
+        self.salary = salary
+    
+        overtime = 0
         if hours_worked >= 50:
             overtime = hours_worked - 50
-            Overtimeamount = (overtime * int((salary / 50)))
-            self.salary += Overtimeamount
+            self.salary = self.salary + (overtime * (self.salary / 50))
+        print(self.salary)
 
         
 
@@ -19,7 +20,10 @@ class Employee:
         self.emp_department = department
     
     def  print_employee_details(self):
-        print(self.emp_id, self.emp_id, self.emp_salary, self.emp_department)
+        print("Employee Name: ", self.emp_name)
+        print("Employee ID: ",self.emp_id)
+        print("Employee Salary: ",self.emp_salary) 
+        print("Employee Department: ",self.emp_department)
         
         
 
@@ -32,11 +36,17 @@ Employee4 = Employee("SMITH", "E7698", 55000, "OPERATIONS")
 
 
 Employee1.emp_assign_department("PERSONNEL")
+Employee4.emp_assign_department("MARKETING")
 
 print(Employee1.emp_department)
+print(Employee2.emp_department)
+print(Employee3.emp_department)
+print(Employee4.emp_department)
 
-
-Employee1.calculate_emp_salary(50000, 60)
-
-print
-
+Employee1.calculate_emp_salary(56000, 600)
+"""
+Employee1.print_employee_details()
+Employee2.print_employee_details()
+Employee3.print_employee_details()
+Employee4.print_employee_details()
+"""
