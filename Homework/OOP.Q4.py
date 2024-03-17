380056a32059256d3fe4614de7a2117b6a5e4a73
Favour Abilogun-Wole Oluwasayo <favyabils125@gmail.com>
2024-03-17 19:36:56 +0100

    New changes

--- Homework/OOP.Q4.py
+++ Homework/OOP.Q4.py
@@ -1,13 +1,18 @@
 w = 4
 class Inventory:
-    def __init__(self):
-         database =  {}
+     database =  {}
          
-    def add_item(self, item_id, item_name, stock_count, price):
-         pass
-    def update_item(self):
-         pass
-    def check_item_details(self):
-        pass
+     def add_item(self, item_id, item_name, stock_count, price):
+         
+         self.database[item_id] = {   #The square bracket defines the data in it to be a key
+               "Item_name": item_name,
+               "Stock_count": stock_count,
+               "Price" : price 
+         } #the database is the main dictionary and the item_id is a key in it,the dictionary connected to the item_id acts as the value to the key  
+     def update_item(self, field, item_id):
+        self.database[item_id].update(field)
+
+     def check_item_details(self, id):
+        return self.database[id]
 
     
